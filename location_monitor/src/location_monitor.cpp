a9e6ff8ee91dbfd5884a79a81e7738a5aa3324e3
salabson <salabson4@yahoo.co.uk>
2019-04-25 11:27:29 +0100

    Create initialize method

--- location_monitor/src/location_monitor.cpp
+++ location_monitor/src/location_monitor.cpp
@@ -1,28 +1,59 @@
+#include<vector>
 #include<string>
+#include "math.h"
 #include "ros/ros.h"
 #include "nav_msgs/Odometry.h"
+#include "location_monitor/LandmarkDistance.h"
 
 using namespace std;
+using location_monitor::LandmarkDistance;
 
 class Landmark{
 public:
-  Landmark():name(name), x(x), y() {}
+  Landmark(string name, double x, double y):name(name), x(x), y() {}
   string name;
-  float x;
-  float y;
+  double x;
+  double y;
 };
 
 class LocationMonitor{
 
 public:
- LocationMonitor(){}
+ LocationMonitor(): landmarks_(){
+  InitLandmarks();
+}
 
 void OdomCallback(const nav_msgs::Odometry::ConstPtr& msg){             
-float x = msg->pose.pose.position.x;                                    
-float y = msg->pose.pose.position.y;                                    
+double  x = msg->pose.pose.position.x;                                    
+double  y = msg->pose.pose.position.y;                                    
 ROS_INFO("X: %f y: %f",x,y);                                            
 }   
 
+LandmarkDistance FindClosest(double x, double y){
+LandmarkDistance result;
+result.distance = -1;
+for(int i=0; i<landmarks_.size(); i++){
+double dx = landmarks_[i].x - x;
+double dy= landmarks_[i].y - y;
+double distance = sqrt(dx*dx-dy*dy);
+if(result.distance<0 | distance < result.distance){
+result.name = landmarks_[i].name;
+result.distance = distance;
+}
+}
+return result;
+}
+
+private:
+  vector<Landmark> landmarks_;
+void InitLandmarks()
+{
+  landmarks_.push_back(Landmark("cube",0.31,-0.99));
+  landmarks_.push_back(Landmark("Dumpster",0.11,-2.42));
+  landmarks_.push_back(Landmark("Cylinder",-1.14,-2.88));
+  landmarks_.push_back(Landmark("Barrier",-2.59,-0.83));
+  landmarks_.push_back(Landmark("Bookshelf",-0.09,0.53));
+}
  };
 
 
